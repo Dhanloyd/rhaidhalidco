c7a81502cc40f9064d4f49fac98c60eff7d6b43f
Dhanloyd <setrojas445@gmail.com>
2026-04-14 09:21:03 +0800

    add vercel cors config

--- api/create-paymongo-checkout.ts
+++ api/create-paymongo-checkout.ts
@@ -2,7 +2,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "https://rhaidhalidco.vercel.app",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
